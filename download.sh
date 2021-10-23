3db38857d82713bfb1043360e02bbe77dd4dcd82
Rishav Pramanik <rishavpramanik@gmail.com>
2021-10-24 02:33:01 +0530

    Update download.sh

--- download.sh
+++ download.sh
@@ -2,4 +2,7 @@
 set -ex
 
 kaggle datasets download -d tawsifurrahman/covid19-radiography-database
-covid19-radiography-database.zip
+git clone https://github.com/ieee8023/covid-chestxray-dataset.git
+kaggle datasets download -d paultimothymooney/chest-xray-pneumonia
+unzip covid19-radiography-database.zip
+unzip chest-xray-pneumonia.zip
